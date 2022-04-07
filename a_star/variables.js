b2403eeb39b7bd3b1969194ff60d66e0836ded59
kosterror <samoylenko.konstantin.3197@gmail.com>
2022-04-07 23:00:37 +0700

    changed colors

--- a_star/variables.js
+++ a_star/variables.js
@@ -1,6 +1,6 @@
-export let activeMode = [0, 0, 0, 0, 0];    
+export let activeMode = [0, 0, 0, 0, 0];
 export let currentSpeed = [0, 0, 0, 0, 0];
-export let delay = { value: 250 };  
+export let delay = { value: 250 };
 
 export let idButtons = ['change_size', 'generate_maze', 'generate_empty_maze', 'add_wall', 'delete_wall', 'change_start', 'change_finish', 'launch', 'speed_1', 'speed_2', 'speed_3', 'speed_4', 'speed_5', 'break'];
 export let idIntrectionButtons = ['add_wall', 'delete_wall', 'change_start', 'change_finish', 'launch'];
@@ -11,9 +11,9 @@ export const COLOR_INACTIVE_BTN = '#8D9091';
 
 export const COLOR_WALL = '#51585a';
 export const COLOR_ROAD = '#bbc6ca';
-export const COLOR_START = 'yellow';
-export const COLOR_FINISH = 'red';
-export const COLOR_PATH = '#6ddf8f';
+export const COLOR_START = '#ECEC17';
+export const COLOR_FINISH = '#F98C4F';
+export const COLOR_PATH = '#8FC7F3';
 export const COLOR_CURRENT_CELL = '#66ff33';
-export const COLOR_CONSIDER_CELL = '#ff99cc';
-export const COLOR_CONSIDERED_CELL = '#6dc2df';
+export const COLOR_CONSIDER_CELL = '#AA43DB';
+export const COLOR_CONSIDERED_CELL = '#247ABF';
